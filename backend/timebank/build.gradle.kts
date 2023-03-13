a03ba113d484fabd26268f9b7a80c310831f548c
Jake <jake@daangn.com>
2023-03-13 09:57:56 +0900

    MySQL 드라이버 의존성 추가

--- backend/timebank/build.gradle.kts
+++ backend/timebank/build.gradle.kts
@@ -16,10 +16,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+
+    runtimeOnly("mysql:mysql-connector-java:8.0.32")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
