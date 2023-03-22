5a8fe097db2c3213c51d03a0f35c4190a522f619
jeonghi <jpark0902@kookmin.ac.kr>
2023-03-23 00:23:34 +0900

    계좌관리관련 오류 핸들러 타입추가

--- backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/application/exception/ApplicationErrorCode.kt
+++ backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/application/exception/ApplicationErrorCode.kt
@@ -10,4 +10,8 @@ enum class ApplicationErrorCode(
     FORBIDDEN("FORBIDDEN", "접근 권한이 없습니다."),
     NOT_FOUND("NOT_FOUND", "존재하지 않는 리소스입니다."),
     INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "서버 내부 오류입니다."),
+
+
+    // 뱅킹 시스템 관련 오류
+    BANK_ERROR("BANK_ERROR", "뱅킹 서버 오류입니다.")
 }
