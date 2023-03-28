2ad7340f6087ed15a68db4a34cde6214b11d702e
jeonghi <jpark0902@kookmin.ac.kr>
2023-03-28 21:44:54 +0900

    [리팩토링] 불필요한 코드 제거

--- backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/domain/repository/BankAccountTransactionJpaRepository.kt
+++ backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/domain/repository/BankAccountTransactionJpaRepository.kt
@@ -1,8 +1,6 @@
 package kookmin.software.capstone2023.timebank.domain.repository
 import kookmin.software.capstone2023.timebank.domain.model.BankAccountTransaction
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import java.time.LocalDateTime
 
 interface BankAccountTransactionJpaRepository : JpaRepository<BankAccountTransaction, Long> {
 }
