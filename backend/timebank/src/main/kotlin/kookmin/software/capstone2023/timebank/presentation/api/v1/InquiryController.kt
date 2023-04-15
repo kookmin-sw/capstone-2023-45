7d2876d04296740ecf34fb753180acbc730ae00b
BbingDdang <39540588+BbingDdang@users.noreply.github.com>
2023-04-15 17:16:52 +0900

    문의 기간 조회 for user update

--- backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/presentation/api/v1/InquiryController.kt
+++ backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/presentation/api/v1/InquiryController.kt
@@ -1,18 +1,22 @@
 package kookmin.software.capstone2023.timebank.presentation.api.v1
 
+import kookmin.software.capstone2023.timebank.application.exception.UnauthorizedException
 import kookmin.software.capstone2023.timebank.application.service.inqui.InquiryService
+import kookmin.software.capstone2023.timebank.domain.model.AccountType
 import kookmin.software.capstone2023.timebank.domain.model.Period
+import kookmin.software.capstone2023.timebank.presentation.api.RequestAttributes
+import kookmin.software.capstone2023.timebank.presentation.api.auth.model.UserContext
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestAttribute
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/inquiries")
@@ -31,16 +35,27 @@ class InquiryController(
      * 문의 전체 조회
      */
     @GetMapping
-    fun getInquiries(): List<InquiryService.InquiryDto> {
+    fun getInquiries(
+        @RequestAttribute(RequestAttributes.USER_CONTEXT) userContext: UserContext,
+    ): List<InquiryService.InquiryDto> {
+        if (userContext.accountType != AccountType.BRANCH) {
+            throw UnauthorizedException(message = "접근 권한이 없습니다.")
+        }
         return inquiryService.getInquiries()
     }
 
     /**
-     * 문의ID겁색 조회 (댓글까지)
+     * 문의ID겁색 조회
      *
      */
     @GetMapping("/{id}")
-    fun getInquiryById(@PathVariable id: Long): InquiryService.InquiryDto {
+    fun getInquiryById(
+        @RequestAttribute(RequestAttributes.USER_CONTEXT) userContext: UserContext,
+        @PathVariable id: Long,
+    ): InquiryService.InquiryDto {
+        if (userContext.accountType != AccountType.BRANCH) {
+            throw UnauthorizedException(message = "접근 권한이 없습니다.")
+        }
         return inquiryService.getInquiryById(id)
     }
 
@@ -48,16 +63,36 @@ class InquiryController(
      * userId검색 조회
      */
     @GetMapping("/users/{userId}")
-    fun getInquiriesByUserId(@PathVariable userId: Long): List<InquiryService.InquiryDto> {
+    fun getInquiriesByUserId(
+        @RequestAttribute(RequestAttributes.USER_CONTEXT) userContext: UserContext,
+        @PathVariable userId: Long
+    ): List<InquiryService.InquiryDto> {
+        if (userContext.accountType == AccountType.INDIVIDUAL) {
+            if (userContext.userId != userId) {
+                throw UnauthorizedException(message = "접근권한이 없습니다.")
+            }
+        }
         return inquiryService.getInquiriesByUserId(userId)
     }
 
     /**
-     * 문의 기간 조회
+     * 문의 기간 조회 for brnach
      */
     @GetMapping("/period")
     fun getInquiriesByPeriod(@RequestParam("period") period: Period): List<InquiryService.InquiryDto> {
         return inquiryService.getInquiriesByPeriod(period)
+    }
+
+    /**
+     * 문의 기간 조회 for user
+     */
+    @GetMapping("/users/{userId}/period")
+    fun getUserInquiriesByPeriod(
+        @RequestAttribute(RequestAttributes.USER_CONTEXT) userContext: UserContext,
+        @RequestParam("period") period: Period,
+        @PathVariable userId: Long
+    ): List<InquiryService.InquiryDto> {
+        return inquiryService.getUserInquiriesByPeriod(period, userId)
     }
 
     /**
