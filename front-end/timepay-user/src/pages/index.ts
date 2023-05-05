149945414a9b93963a4dda7a07d0fe94f5be126f
p3chaeyeon <p3chaeyeon@gmail.com>
2023-05-05 20:13:46 +0900

    페이지 연결

--- front-end/timepay-user/src/pages/index.ts
+++ front-end/timepay-user/src/pages/index.ts
@@ -1,16 +1,16 @@
-import HomePage from './HomePage';
-import MyPage from './MyPage';
-import SearchPage from './SearchPage';
-import IntroPage from './IntroPage';
-import {SignUp, Password} from './SignUp';
-import {UserMainPage, ProfileEdit, Unregistal} from './MainPage';
-import Transfer from './Transfer';
-import TransferAmt from './Transfer/TransferAmount';
-import TransferPW from './Transfer/TransferPassword';
-import TransferLog from './Transfer/TransferLog';
-import QnaRegister from './Qna/QnaRegister';
-import QnaLogMain from './Qna/QnaLogMain';
-import QnaLogDetail from './Qna/QnaLogDetail';
+import HomePage from "./HomePage";
+import MyPage from "./MyPage";
+import SearchPage from "./SearchPage";
+import IntroPage from "./IntroPage";
+import { SignUp, Password } from "./SignUp";
+import { UserMainPage, Unregistal, ProfileEdit } from "./MainPage";
+import Transfer from "./Transfer";
+import TransferAmt from "./Transfer/TransferAmount";
+import TransferPW from "./Transfer/TransferPassword";
+import TransferLog from "./Transfer/TransferLog";
+import QnaRegister from "./Qna/QnaRegister";
+import QnaLogMain from "./Qna/QnaLogMain";
+import QnaLogDetail from "./Qna/QnaLogDetail";
 
 const PageRoutes = {
   IntroPage,
@@ -29,7 +29,6 @@ const PageRoutes = {
   QnaRegister,
   QnaLogMain,
   QnaLogDetail,
-
 };
 
 export default PageRoutes;
